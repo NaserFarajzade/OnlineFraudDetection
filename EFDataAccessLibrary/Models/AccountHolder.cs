989e19ad270a3f9b3b0a05900f114afcbf99af52
Naser Farajzadeh <n.farajzadeh@mohaymen.ir>
2022-08-11 23:34:43 +0430

    db models

---
+++ EFDataAccessLibrary/Models/AccountHolder.cs
@@ -0,0 +1,19 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace EFDataAccessLibrary.Models;
+
+public class AccountHolder
+{
+    public int Id { get; set; }
+    
+    [MaxLength(100)]
+    public string Name { get; set; }
+    
+    [Required]
+    [MaxLength(10)]
+    public string NationalCode { get; set; }
+    
+    [Required]
+    [MaxLength(16)]
+    public string CardNumber { get; set; }
+}
---
+++ EFDataAccessLibrary/Models/Profile.cs
@@ -0,0 +1,14 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace EFDataAccessLibrary.Models;
+
+public class Profile
+{
+    public int Id { get; set; }
+    
+    [Required]
+    [MaxLength(16)]
+    public string CardNumber { get; set; }
+    public long TransactionsAmountAverage { get; set; }
+    public int AccountHolderCardsCount { get; set; }
+}
