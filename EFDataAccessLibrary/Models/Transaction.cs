2e0d1191da34a68d7dee785951315564f3fa17e7
Naser Farajzadeh <n.farajzadeh@mohaymen.ir>
2022-08-11 23:33:41 +0430

    git ignore

---
+++ EFDataAccessLibrary/Models/Transaction.cs
@@ -0,0 +1,21 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace EFDataAccessLibrary.Models;
+
+public class Transaction
+{
+    public int Id { get; set; }
+    
+    [Required]
+    [MaxLength(16)]
+    public string OriginCard { get; set; }
+    
+    [Required]
+    [MaxLength(16)]
+    public string DestinationCard { get; set; }
+    
+    public DateTime TransactionDateTime { get; set; }
+    public long TransactionAmount { get; set; }
+    public string TransactionReference { get; set; }
+    public string TransactionId { get; set; }
+}
