5b29ff2b6e09a3155c280d79b92f6ccca305a17f
Naser Farajzadeh <n.farajzadeh@mohaymen.ir>
2022-08-11 23:36:31 +0430

    add db context

---
+++ EFDataAccessLibrary/DataAccess/DataBaseContext.cs
@@ -0,0 +1,13 @@
+﻿using EFDataAccessLibrary.Models;
+using Microsoft.EntityFrameworkCore;
+
+namespace EFDataAccessLibrary.DataAccess;
+
+public class DataBaseContext: DbContext
+{
+    public DataBaseContext(DbContextOptions options): base(options){}
+    
+    public DbSet<AccountHolder> AccountHolders { get; set; }
+    public DbSet<Transaction> Transactions { get; set; }
+    public DbSet<Profile> Profiles { get; set; }
+}
--- OnlineFraudDetection/Program.cs
+++ OnlineFraudDetection/Program.cs
@@ -1,11 +1,21 @@
+using EFDataAccessLibrary.DataAccess;
+using Microsoft.EntityFrameworkCore;
+using OnlineFraudDetection.Models;
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
 
 builder.Services.AddControllers();
+builder.Services.AddDbContext<DataBaseContext>(options =>
+{
+    options.UseSqlServer(builder.Configuration.GetConnectionString("Default") ?? throw new Exception("Database not found"));
+});
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
+var settings = builder.Configuration.GetSection("Settings").Get<Settings>();
+builder.Services.AddSingleton<Settings>(settings);
 
 var app = builder.Build();
 
