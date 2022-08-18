809faea3bb0a07936e87d741ac6660a00ed31366
Naser Farajzadeh <n.farajzadeh@mohaymen.ir>
2022-08-18 22:58:12 +0430

    add configurations

--- OnlineFraudDetection/Models/Configuration.cs
+++ OnlineFraudDetection/Models/Configuration.cs
@@ -4,23 +4,47 @@ public class Settings
 {
     public string TransactionFolder { get; set; }
     public string AccountHolderFolder { get; set; }
+    public char SeparatorCharacter { get; set; } = ',';
+    public int BulkSize { get; set; } = 1;
     public int AccountHolderMaximumCardsCount { get; set; }
     public int ToleranceCoefficient { get; set; }
     public List<string> SuspiciousBanks { get; set; }
     public List<int> FraudulentTransactionTimes { get; set; }
     public TransactionFieldsIndexSettings TransactionFieldsIndex { get; set; }
     public AccountHoldersFieldsIndexSettings AccountHoldersFieldsIndex { get; set; }
+    public FraudulentFactorCoefficientSettings FraudulentFactorCoefficient { get; set; }
+    public FraudulentFactorNormalizationSettings FraudulentFactorNormalizationValue { get; set; }
+    public RedisCache redisCache { get; set; }
+}
+
+public class RedisCache
+{
+    public bool EnableCaching { get; set; }
+    public string IP { get; set; }
+    public int Port { get; set; }
+}
+
+public class FraudulentFactorNormalizationSettings
+{
+    public double CardsCount {get; set;} 
+    public double ExceedingTheAverage {get; set;}    
+}
+
+public class FraudulentFactorCoefficientSettings
+{
+    public int Time {get; set;}
+    public int CardsCount {get; set;}
+    public int BankType {get; set;}
+    public int ExceedingTheAverage {get; set;}
 }
 
 public class TransactionFieldsIndexSettings
 {
     public int OriginCard {get; set;}
     public int DestinationCard {get; set;}
-    public int TransactionDate {get; set;}
-    public int TransactionTime {get; set;}
+    public int TransactionDateTime {get; set;}
     public int TransactionAmount {get; set;}
     public int TransactionReference {get; set;}
-    public int TransactionId {get; set;}
     
 }
 
