03726ef0d3ee1ba811835e06a8289acddf5defe3
Naser Farajzadeh <n.farajzadeh@mohaymen.ir>
2022-08-18 22:59:16 +0430

    sigmoid function

---
+++ OnlineFraudDetection/Validators/Implementation/Utility/SigmoidFunction.cs
@@ -0,0 +1,10 @@
+﻿namespace OnlineFraudDetection.Validators.Implementation.Utility;
+
+public static class SigmoidFunction
+{
+    public static int Calculate(double input, double normalizationFactor, double avg)
+    {
+        var power = (-6 * (input - avg)) / (normalizationFactor * avg);
+        return (int)((200 / (1 + Math.Pow(Math.E, power))) - 100);
+    }
+}
